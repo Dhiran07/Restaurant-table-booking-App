9c4c2dd51e5c2cbeb60b0bddda22822580470cd1
Dhiran <97387022+dhiran-dev@users.noreply.github.com>
2023-02-01 13:29:21 +0530

    completed Hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "../components/Hero.css";
+import RestImage from "../images/restauranfood.jpg";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <>
+      <div className="hero">
+        <section className="left">
+          <h1>Little Lemon</h1>
+          <h4>Chicago</h4>
+          <p>
+            We are a family owned Mediterranean restaurant, focused on
+            traditional recipes served with a modern twist.
+          </p>
+          <Link to="/reservations">
+            <button className="btn">Reserve a Table</button>
+          </Link>
+        </section>
+        <section className="right">
+          <img src={RestImage} alt="food" />
+        </section>
+      </div>
+    </>
+  );
 };
 
 export default Hero;
