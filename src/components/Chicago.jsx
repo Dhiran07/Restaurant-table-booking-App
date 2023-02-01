96d64574fd07b63420c5353497a65622dcfc7ebd
Dhiran <97387022+dhiran-dev@users.noreply.github.com>
2023-02-02 01:08:57 +0530

    Completed HomePage
    Mobile hamburger menu is still pending

--- src/components/Chicago.jsx
+++ src/components/Chicago.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import img1 from "../images/Mario and Adrian A.jpg";
+import img2 from "../images/Mario and Adrian b.jpg";
+import './Chicago.css'
 
 const Chicago = () => {
-  return <div>Chicago</div>;
+  return (
+    <>
+      <div className="chicago">
+        <section className="left-chicago">
+          <h2>Little Lemon</h2>
+          <h3>Chicago</h3>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eaque,
+            veritatis iusto voluptatum ad labore sunt officiis nam excepturi non
+            dignissimos, dolorum laborum quis, deserunt eum commodi. Quas,
+            possimus! Totam, repellat.
+          </p>
+        </section>
+        <section className="right-chicago">
+          <img src={img1} alt="chef" className="img1" />
+          <img src={img2} alt="chef" className="img2" />
+        </section>
+      </div>
+    </>
+  );
 };
 
 export default Chicago;
--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,7 +1,110 @@
 import React from "react";
+import { AiFillStar } from "react-icons/ai";
+import avatar1 from "../images/avatar1.jpg";
+import avatar2 from "../images/avatar2.jpg";
+import avatar3 from "../images/avatar3.jpg";
+import avatar4 from "../images/avatar4.jpg";
+import "./Testimonials.css";
 
 const Testimonials = () => {
-  return <div>Testimonials</div>;
+  return (
+    <>
+      <section className="testimonials">
+        <h1>Testimonials</h1>
+
+        <section className="test-cards">
+          <div className="test-card">
+            <div className="test-star">
+              <AiFillStar />
+              <AiFillStar />
+              <AiFillStar />
+              <AiFillStar />
+              <AiFillStar />
+            </div>
+
+            <div className="test-profile">
+              <img src={avatar1} alt="" />
+              <h4>Samir</h4>
+            </div>
+
+            <p className="review">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eaque,
+              veritatis iusto voluptatum ad labore sunt officiis nam excepturi
+              non dignissimos, dolorum laborum quis, deserunt eum commodi. Quas,
+              possimus! Totam, repellat.
+            </p>
+          </div>
+          {/* Start of test 2 */}
+          <div className="test-card">
+            <div className="test-star">
+              <AiFillStar />
+              <AiFillStar />
+              <AiFillStar />
+              <AiFillStar />
+              <AiFillStar />
+            </div>
+
+            <div className="test-profile">
+              <img src={avatar2} alt="" />
+              <h4>Yusron</h4>
+            </div>
+
+            <p className="review">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eaque,
+              veritatis iusto voluptatum ad labore sunt officiis nam excepturi
+              non dignissimos, dolorum laborum quis, deserunt eum commodi. Quas,
+              possimus! Totam, repellat.
+            </p>
+          </div>
+          {/* Start of test 3 */}
+          <div className="test-card">
+            <div className="test-star">
+              <AiFillStar />
+              <AiFillStar />
+              <AiFillStar />
+              <AiFillStar />
+              <AiFillStar />
+            </div>
+
+            <div className="test-profile">
+              <img src={avatar3} alt="" />
+              <h4>Shami</h4>
+            </div>
+
+            <p className="review">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eaque,
+              veritatis iusto voluptatum ad labore sunt officiis nam excepturi
+              non dignissimos, dolorum laborum quis, deserunt eum commodi. Quas,
+              possimus! Totam, repellat.
+            </p>
+          </div>
+
+          {/* Start of test 4 */}
+          <div className="test-card">
+            <div className="test-star">
+              <AiFillStar />
+              <AiFillStar />
+              <AiFillStar />
+              <AiFillStar />
+              <AiFillStar />
+            </div>
+
+            <div className="test-profile">
+              <img src={avatar4} alt="" />
+              <h4>Aleem</h4>
+            </div>
+
+            <p className="review">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eaque,
+              veritatis iusto voluptatum ad labore sunt officiis nam excepturi
+              non dignissimos, dolorum laborum quis, deserunt eum commodi. Quas,
+              possimus! Totam, repellat.
+            </p>
+          </div>
+        </section>
+      </section>
+    </>
+  );
 };
 
 export default Testimonials;
